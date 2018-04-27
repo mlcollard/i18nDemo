3d2b62c843ee34003361260b38c95c972761cbca
Michael L. Collard <collard@uakron.edu>
2018-04-27 12:02:04 -0400

    Initial Commit

---
+++ i18nDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  i18nDemo
+//
+//  Created by Michael L. Collard on 4/27/18.
+//  Copyright © 2018 Michael L. Collard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
